90f806a6bcbc66744c3a01f78792215acdc370ad
gtasa3box <gtasa3box@gmail.com>
2021-08-14 08:02:44 +0300

    adaptiv

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,7 @@
   * @param {number} first - первое число
   * @returns {number}
   */
+new fullpage('#fullpage', {
+  anchors: ['page1', 'page2', 'page3', 'page4'],
+  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+});
