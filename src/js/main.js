331bb7fb6ec26f063d2d3ca2c7f4bb2d6eb6e7c6
gtasa3box <gtasa3box@gmail.com>
2021-08-17 00:05:34 +0300

    page

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,31 @@
   * @param {number} first - первое число
   * @returns {number}
   */
-new fullpage('#fullpage', {
-  anchors: ['page1', 'page2', 'page3', 'page4'],
-  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-});
+
+window.onload= function() {
+      document.getElementById('burgerlink').onclick = function() {
+      toogleclass(this);
+      opennav('nav', this);
+      return false;
+  };
+};
+
+function opennav(id, click_burger) {
+const div = document.getElementById(id);
+    if (nav.classList.contains('active')) {
+        nav.classList.remove('active');
+    }
+    else {
+        nav.classList.add('active');
+    }
+};
+
+function toogleclass(click_burger) {
+    if (click_burger.classList.contains('active')) {
+        click_burger.classList.remove('active');
+    }
+    else {
+        click_burger.classList.add('active');
+    }
+};
+
