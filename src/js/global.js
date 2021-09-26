a8efc65408e93c48ce4a1030068e5a826ad6b506
gtasa3box <gtasa3box@gmail.com>
2021-09-26 19:31:17 +0300

    g

--- src/js/global.js
+++ src/js/global.js
@@ -1,2 +1,12 @@
-console.log('global');
+$(function () {
+    $("form input[type='radio']").change(function() {
+        if ($('#elementary').is(":checked")){
+            $('#protected').prop('disabled', true);
+        }
+        else {
+            $('#protected').prop('disabled', false);
+        }
+    });
+});
 
+$(".phone_mask").mask("+7 (999) 999-9999");
